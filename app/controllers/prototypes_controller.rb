59c6f5c613638ac9250dfcfb7cd5b1e556ad70a1
to-yan <t.satoru03@gmail.com>
2021-12-16 01:05:32 +0900

    作業履歴

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -40,6 +40,17 @@ class PrototypesController < ApplicationController
     end
   end
 
+  # def update
+  #   prototype = Prototype.find(params[:id])
+  #   prototype.update(prototype_params)
+  #   if prototype.save
+  #     redirect_to prototype_path
+  #   else
+  #     render :edit
+  #   end
+  # end
+
+
   def destroy
     prototype = Prototype.find(params[:id])
     prototype.destroy
